485f42fab2d3d0e198ef98e19046a3876fdf47ac
Oigen B <eugen.begiqi@gmail.com>
2021-07-24 00:21:24 +0200

    first commit

---
+++ src/main/java/com/project/tutorial/garofano/Garofano1Application.java
@@ -0,0 +1,13 @@
+package com.project.tutorial.garofano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Garofano1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Garofano1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/tutorial/garofano/Garofano1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tutorial.garofano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Garofano1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
